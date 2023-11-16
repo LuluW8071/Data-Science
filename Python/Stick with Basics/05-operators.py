ba7aaa8b8c676e2335670e8a0a25683d051293cf
luluw <shashinmhrzn@gmail.com>
2023-11-16 21:32:54 +0545

    Day1: Python Basics

---
+++ Python/Stick with Basics/05-operators.py
@@ -0,0 +1,16 @@
+print(4 + 5)    # Addiiton Operator '+'
+print(4 - 5)    # Subtraction Operator '-'
+print(4 * 5)    # Multiplication Operator '*'
+print(4 / 5)    # Division Operator '/'
+print(4 ** 3)   # Exponential Operator '**' : 4*4*4
+print(34 // 5)  # Float Division Operator '//'
+print(46 % 17)  # Modulus Operator '%'
+
+# Output:
+# 9
+# -1
+# 20
+# 0.8
+# 64
+# 6
+# 12
---
+++ Python/Stick with Basics/05E-Exercise1.py
@@ -0,0 +1,16 @@
+# Create a calculator capable of add, subtract, multiplicaiton, and division of any two numbers
+'''Using only things that we learned from 01-05*.py'''
+
+a = 35
+b = 12
+
+print('The value of', a, '+', b, 'is', a + b)
+print('The value of', a, '-', b, 'is', a - b)
+print('The value of', a, '*', b, 'is', a * b)
+print('The value of', a, '/', b, 'is', a / b)
+
+# Output:
+# The value of 35 + 12 is 47
+# The value of 35 - 12 is 23
+# The value of 35 * 12 is 420
+# The value of 35 / 12 is 2.9166666666666665
