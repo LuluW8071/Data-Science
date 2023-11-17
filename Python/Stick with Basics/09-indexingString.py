47ced8e10f3626caf489953292a8bd6565f89704
luluw <shashinmhrzn@gmail.com>
2023-11-17 11:22:11 +0545

    Day 2: Python Basics

---
+++ Python/Stick with Basics/09-indexingString.py
@@ -0,0 +1,42 @@
+name = 'Python'
+name1 = 'Python is a great language'
+
+print(name[0])
+print(name[1])
+# print(name[6])  # IndexError: string index out of range
+
+# Looping through the string
+print('--------Looping--------')
+for characters in name1:
+    print(characters)
+
+# Output:
+# P
+# y
+# --------Looping--------
+# P
+# y
+# t
+# h
+# o
+# n
+
+# i
+# s
+
+# a
+
+# g
+# r
+# e
+# a
+# t
+
+# l
+# a
+# n
+# g
+# u
+# a
+# g
+# e
