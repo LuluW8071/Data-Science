26a5f4781b41374054700d8834314555f4943cc2
Shashin <shashinmhrzn@gmail.com>
2024-02-26 13:49:51 +0000

    [Typo fix]

--- Python/Scripts/Basics-00/04-list_Tuple&Dict.py
+++ Python/Scripts/Basics-00/04-list_Tuple&Dict.py
@@ -1,17 +1,19 @@
 # List is an ordered collection of data with elements separated by commas and enclosed within square brackets
-# These lists can be modified aafter creation
+# These lists can be modified after creation
 
 list1 = [1, 2, 3, [-2, -3], ['a', 'b', 'c']]
 print(list1)
 
 # Tuple is an ordered collection of data with elements separated by commas and enclosed within paranthesis
-# Tuples can't be modified aafter creation
+# Tuples can't be modified after creation
 
 tuple1 = (3, 4, 5, (-4, -5), ('x', 'y', 'z'))
 print(tuple1)
 
 # Dictionary(Mapped Data) is an unordered collection of data containing key:value pairs enclosed within curly brackets
-dict1 = {'name': 'Python', 'age': 2023, 'language': True}
+dict1 = {'name': 'Python', 
+         'age': 2023, 
+         'language': True}
 print(dict1)
 
 # Output:
